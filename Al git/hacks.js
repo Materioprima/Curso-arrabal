104d476bf4caa830b28b035f06b82bb9e98e05f0
Mateo Burbano <mateoburbano03@gmail.com>
2018-01-11 19:09:57 +0100

    Cambios optimizacion

--- Al git/hacks.js
+++ Al git/hacks.js
@@ -16,42 +16,31 @@ function proceso(valor){
 		cpu=Math.floor(Math.random()*3);
 		if(valor==0 && cpu==2){
 				victorias++;
-				rondas++;
 				res.text('Has ganado');
-				fin(rondas);
 		}else if (valor==1 && cpu==0){
 				victorias++;
-				rondas++;
 				res.text('Has ganado');
-				fin(rondas);
 		}else if(valor==2 && cpu==1){
 				victorias++;
-				rondas++;
 				res.text('Has ganado');
-				fin(rondas);
 		}else if (valor==cpu) {
 			rondas++;
 			res.text('Empate');
-			fin(rondas);
 		}else{
-			rondas++;
 			maquina++;
 			res.text('Has perdido');
-			fin(rondas);
+		}
+		if(victorias==3){
+			res.text('Felicidades, has ganado a la CPU.');
+			bienvenid.text('Fin del juego');
+		}else if(maquina==3){
+			res.text('Has perdido, vuelve a intentarlo');
+			bienvenid.text('Fin del juego');
 		}
 		marca.text(victorias+'-'+maquina);
 		iajugada.text('La CPU ha jugado:');
 		imag.attr('src',lista[cpu]);
 
-	}
-	function fin(round){
-		if(victorias==3){
-			res.text('Felicidades, has ganado a la CPU.');
-			}else{
-				res.text('Ha ganado la CPU, vuelve a intentarlo');
-			}
-			bienvenid.text('Fin del juego');
-		}
 	}
 	hola();
 	$(".cl1").click(function(){
